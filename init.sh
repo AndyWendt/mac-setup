9b3ca5c46c68b6c998ea8b9e445ee147af0a6e8f
Andy Wendt <awendt@mavenlink.com>
2019-10-07 19:51:28 -0600

    Initial commit

---
+++ init.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+brew cask install slack
+brew cask install authy
+brew cask install iterm2
+brew cask install jetbrains-toolbox
+brew cask install sequel-pro
+brew cask install visual-studio-code
+
+chmod 0600 ~/.ssh/id_rsa
+
+defaults write com.apple.dock orientation -string left && killall Dock
+defaults write com.apple.dock autohide-delay -float 0; killall Dock
+defaults write com.apple.dock persistent-apps -array
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Disable Resume system-wide
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# set touchbar default to FKeys
+defaults write com.apple.touchbar.agent PresentationModeGlobal functionKeys; sudo pkill TouchBarServer
+
+

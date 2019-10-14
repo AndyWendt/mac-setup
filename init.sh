b5a1353cab38139e87cfe3a3d29aeb34480b25d3
Andy Wendt <awendt@mavenlink.com>
2019-10-14 11:34:27 -0600

    Playing around with night shift settings

--- init.sh
+++ init.sh
@@ -6,7 +6,8 @@ ansible-playbook ./init.yml
 # https://github.com/LukeChannings/.config/blob/master/install.macos#L418-L438
 # sudo defaults read com.apple.CoreBrightness.plist
 
-CORE_BRIGHTNESS="/var/root/Library/Preferences/com.apple.CoreBrightness.plist"
+# CORE_BRIGHTNESS="/var/root/Library/Preferences/com.apple.CoreBrightness.plist"
+CORE_BRIGHTNESS="com.apple.corebrightness"
 
 ENABLE='{
   CBBlueLightReductionCCTTargetRaw = 2700;
@@ -25,10 +26,13 @@ ENABLE='{
     BlueReductionSunScheduleAllowed = 1;
     Version = 1;
   };
+  CoreAnalyticsTimestamps =     {
+    "com.apple.corebrightness.harmony.state" = "2019-10-12 02:33:53 +0000";
+  };
 }'
 
 echo "Changing night shift settings"
-sudo defaults write $CORE_BRIGHTNESS "CBUser-0" "$ENABLE"
+# sudo defaults write $CORE_BRIGHTNESS "CBUser-0" "$ENABLE"
 sudo defaults write $CORE_BRIGHTNESS "CBUser-$(dscl . -read $HOME GeneratedUID | sed 's/GeneratedUID: //')" "$ENABLE"
 
 echo "Restarting the menubar"

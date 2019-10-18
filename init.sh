5c941196fbcdb679a7931b991de5837390e64de0
Andy Wendt <awendt@mavenlink.com>
2019-10-18 16:58:42 -0600

    Comment out night shift settings

--- init.sh
+++ init.sh
@@ -6,34 +6,35 @@ ansible-playbook ./init.yml
 # https://github.com/LukeChannings/.config/blob/master/install.macos#L418-L438
 # sudo defaults read com.apple.CoreBrightness.plist
 
+# Night Shift settings don't work right now
 # CORE_BRIGHTNESS="/var/root/Library/Preferences/com.apple.CoreBrightness.plist"
-CORE_BRIGHTNESS="com.apple.corebrightness"
+#CORE_BRIGHTNESS="com.apple.corebrightness"
+#
+#ENABLE='{
+#  CBBlueLightReductionCCTTargetRaw = 2700;
+#  CBBlueReductionStatus =     {
+#    AutoBlueReductionEnabled = 1;
+#    BlueLightReductionDisableScheduleAlertCounter = 3;
+#    BlueLightReductionSchedule = {
+#      DayStartHour = 7;
+#      DayStartMinute = 0;
+#      NightStartHour = 7;
+#      NightStartMinute = 1;
+#    };
+#    BlueReductionAvailable = 1;
+#    BlueReductionEnabled = 1;
+#    BlueReductionMode = 2;
+#    BlueReductionSunScheduleAllowed = 1;
+#    Version = 1;
+#  };
+#  CoreAnalyticsTimestamps =     {
+#    "com.apple.corebrightness.harmony.state" = "2019-10-12 02:33:53 +0000";
+#  };
+#}'
 
-ENABLE='{
-  CBBlueLightReductionCCTTargetRaw = 2700;
-  CBBlueReductionStatus =     {
-    AutoBlueReductionEnabled = 1;
-    BlueLightReductionDisableScheduleAlertCounter = 3;
-    BlueLightReductionSchedule = {
-      DayStartHour = 7;
-      DayStartMinute = 0;
-      NightStartHour = 7;
-      NightStartMinute = 1;
-    };
-    BlueReductionAvailable = 1;
-    BlueReductionEnabled = 1;
-    BlueReductionMode = 2;
-    BlueReductionSunScheduleAllowed = 1;
-    Version = 1;
-  };
-  CoreAnalyticsTimestamps =     {
-    "com.apple.corebrightness.harmony.state" = "2019-10-12 02:33:53 +0000";
-  };
-}'
-
-echo "Changing night shift settings"
+#echo "Changing night shift settings"
 # sudo defaults write $CORE_BRIGHTNESS "CBUser-0" "$ENABLE"
-sudo defaults write $CORE_BRIGHTNESS "CBUser-$(dscl . -read $HOME GeneratedUID | sed 's/GeneratedUID: //')" "$ENABLE"
+#sudo defaults write $CORE_BRIGHTNESS "CBUser-$(dscl . -read $HOME GeneratedUID | sed 's/GeneratedUID: //')" "$ENABLE"
 
 echo "Installing Tuple"
 curl -L https://git.io/tuple-install | bash
